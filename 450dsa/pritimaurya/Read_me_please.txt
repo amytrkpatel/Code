0d74fa3d63e76128f7f756f4b37fb93afb60a46e
5m6gP74sM85fVPN <amytrkpatel20@gmail.com>
2022-01-28 17:05:56 +0530

    Added Problem Links

--- 450dsa/pritimaurya/Read_me_please.txt
+++ 450dsa/pritimaurya/Read_me_please.txt
@@ -4,8 +4,14 @@ Visit https://450dsa.com/ to track your Progress, Make folders of Each section.
 
 pritimaurya/array/
 pritimaurya/matrix/
+.
+.
+.
 
 NOTE: use underscore _ to connect two words instead of space or dash -
 
 
 Lets do this!
+
+Solution NAMING FORMAT: Qno_two_three_words_to_identify
+Any FILE NAMING FORMAT: use_underscore_between
--- Common_elements_logic6.cpp
+++ Common_elements_logic6.cpp
@@ -1,43 +1,43 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
 
     int t;
-    cin>>t;
-    while(t--){
+    cin >> t;
+    while (t--)
+    {
         int n1, n2, n3;
-        cin>>n1>>n2>>n3;
+        cin >> n1 >> n2 >> n3;
 
         int64_t a[n1], b[n2], c[n3];
 
-
-        for(int i = 0; i < n1; i++){
-            cin>>a[i];
+        for (int i = 0; i < n1; i++)
+        {
+            cin >> a[i];
         }
 
-        for(int i = 0; i < n2; i++){
-            cin>>b[i];
+        for (int i = 0; i < n2; i++)
+        {
+            cin >> b[i];
         }
 
-        for(int i = 0; i < n3; i++){
-            cin>>c[i];
+        for (int i = 0; i < n3; i++)
+        {
+            cin >> c[i];
         }
 
         int i = 0, j = 0, k = 0, flag = 0, l = 0;
         int64_t z[l];
 
-
-        while(i < n1 && j < n2 && k < n3)
+        while (i < n1 && j < n2 && k < n3)
         {
 
-
-            if(a[i]==b[j] && b[j] == c[k] && a[i] == c[k])
+            if (a[i] == b[j] && b[j] == c[k] && a[i] == c[k])
             {
                 flag = 1;
 
-
-                z[l]=a[i];
+                z[l] = a[i];
                 l++;
 
 
@@ -47,41 +47,40 @@ int main()
 
             }
             else if (a[i] < b[j])
-             i++;
+                i++;
 
-         // y < z
+            // y < z
             else if (b[j] < c[k])
-             j++;
+                j++;
 
-         // We reach here when x > y and z < y, i.e., z is smallest
-            else if(c[k] < a[i])
-             k++;
-
-
-
+            // We reach here when x > y and z < y, i.e., z is smallest
+            else if (c[k] < a[i])
+                k++;
         }
-        for(int64_t x = 0; x < l; x++)
+        for (int64_t x = 0; x < l; x++)
         {
-            if(x==0){
-                cout<<z[x]<<" ";
+            if (x == 0)
+            {
+                cout << z[x] << " ";
             }
-            else{
-                if(z[x] == z[x-1])
-                x++;
-            else{
-                cout<<z[x]<<" ";
-                x++;
+            else
+            {
+                if (z[x] == z[x - 1])
+                    x++;
+                else
+                {
+                    cout << z[x] << " ";
+                    x++;
+                }
             }
         }
 
-        }
-
-        if(flag == 0)
+        if (flag == 0)
         {
 
-            cout<<"-1";
+            cout << "-1";
         }
-        cout<<"\n";
+        cout << "\n";
     }
     return 0;
 }
