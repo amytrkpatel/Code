ec435c213983cad5d0400cd473fbfcc2d865b48e
Amit Patel <amytrkpatel20@gmail.com>
2025-02-15 22:49:31 +0530

    Time: 11 ms (18.33%) | Memory: 17.9 MB (54.10%) - LeetSync

--- LeetCode/Web/205-isomorphic-strings/isomorphic-strings.py
+++ LeetCode/Web/205-isomorphic-strings/isomorphic-strings.py
@@ -3,27 +3,14 @@ class Solution:
         if len(s) != len(t):
             return False
 
-        hash_s = {}
-        hash_t = {}
+        hash_s = {}  # Maps s[i] -> t[i]
+        hash_t = {}  # Maps t[i] -> s[i]
+
         for i in range(len(s)):
-            if s[i] not in hash_s:
-                hash_s[s[i]] = set(t[i])
-            else:
-                hash_s[s[i]].add(t[i])
-            
-            if t[i] not in hash_t:
-                hash_t[t[i]] = set(s[i])
-            else:
-                hash_t[t[i]].add(s[i])
-        
-        for v in hash_s.values():
-            if len(v) > 1:
+            if (s[i] in hash_s and hash_s[s[i]] != t[i]) or (t[i] in hash_t and hash_t[t[i]] != s[i]):
                 return False
-        
-        for v in hash_t.values():
-            if len(v) > 1:
-                return False
-        
-        print(hash_s)
-        print(hash_t)
+
+            hash_s[s[i]] = t[i]
+            hash_t[t[i]] = s[i]
+
         return True
