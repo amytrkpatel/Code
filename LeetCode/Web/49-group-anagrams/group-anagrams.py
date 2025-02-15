ddd11306e687a8f2dad8d488ac2e9ced0f4dbb22
Amit Patel <amytrkpatel20@gmail.com>
2025-02-15 23:06:50 +0530

    Time: 11 ms (83.70%) | Memory: 20.5 MB (95.18%) - LeetSync

--- LeetCode/Web/49-group-anagrams/group-anagrams.py
+++ LeetCode/Web/49-group-anagrams/group-anagrams.py
@@ -1,14 +1,12 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         if len(strs) == 0:
-            return [strs]
+            return [[]]  # Fix: Empty list inside a list
         
-        dict_of_anagrams = {}
-        
-        for i in range(0, len(strs)):
-            if ''.join(sorted(strs[i])) not in dict_of_anagrams.keys():
-                dict_of_anagrams[''.join(sorted(strs[i]))] = [strs[i]]
-            else:
-                dict_of_anagrams[''.join(sorted(strs[i]))].append(strs[i])
-        result = [value for key, value in dict_of_anagrams.items()]
-        return result
+        dict_of_anagrams = defaultdict(list)  # Fix: defaultdict(list) instead of defaultdict(str)
+
+        for word in strs:
+            sorted_word = ''.join(sorted(word))  # Sort the word
+            dict_of_anagrams[sorted_word].append(word)  # Group anagrams together
+
+        return list(dict_of_anagrams.values())  # Return grouped anagrams
