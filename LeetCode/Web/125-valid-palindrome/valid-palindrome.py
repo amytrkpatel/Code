1d65d2f92aa2f33fd0925af9ecdc222306b82d12
Amit Patel <amytrkpatel20@gmail.com>
2025-02-16 09:21:00 +0530

    Time: 11 ms (36.66%) | Memory: 17.8 MB (95.15%) - LeetSync

--- LeetCode/Web/125-valid-palindrome/valid-palindrome.py
+++ LeetCode/Web/125-valid-palindrome/valid-palindrome.py
@@ -8,13 +8,34 @@ class Solution:
 
         #Approach 2: Two Opposite Direction Pointer method
         #-------------------------------------------------
-        pal_s = ''.join(x.lower() for x in s if x.isalnum())
+        # pal_s = ''.join(x.lower() for x in s if x.isalnum())
 
-        start, end = 0, len(pal_s)-1
+        # start, end = 0, len(pal_s)-1
+        # while start < end:
+        #     if pal_s[start] == pal_s[end]:
+        #         start += 1
+        #         end -= 1
+        #     else:
+        #         return False
+        # return True
+
+        #Approach 3: Space Efficient
+        #-------------------------------------------------
+        start, end = 0, len(s) - 1
+
         while start < end:
-            if pal_s[start] == pal_s[end]:
+            # Move start if it's not alphanumeric
+            while start < end and not s[start].isalnum():
                 start += 1
+            # Move end if it's not alphanumeric
+            while start < end and not s[end].isalnum():
                 end -= 1
-            else:
+            
+            # Compare characters (ignoring case)
+            if s[start].lower() != s[end].lower():
                 return False
+            
+            start += 1
+            end -= 1
+
         return True
