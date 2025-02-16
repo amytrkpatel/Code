047df9fb56a03614ae699d90099eeb2338765001
Amit Patel <amytrkpatel20@gmail.com>
2025-02-16 09:13:53 +0530

    Time: 9 ms (46.36%) | Memory: 23.3 MB (6.08%) - LeetSync

--- LeetCode/Web/125-valid-palindrome/valid-palindrome.py
+++ LeetCode/Web/125-valid-palindrome/valid-palindrome.py
@@ -1,4 +1,20 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        
+        # Approach 1: Using Python to reverse the string and then matching
+        #-------------------------------------------------------------------------------------
+        # pal_s = ''.join(x.lower() for x in s if x.isalnum())
+        # return pal_s == pal_s[::-1] #return if palindrome or not
+
+        #Approach 2: Two Opposite Direction Pointer method
+        #-------------------------------------------------
         pal_s = ''.join(x.lower() for x in s if x.isalnum())
-        return pal_s == pal_s[::-1] #return if palindrome or not
+
+        start, end = 0, len(pal_s)-1
+        while start < end:
+            if pal_s[start] == pal_s[end]:
+                start += 1
+                end -= 1
+            else:
+                return False
+        return True
