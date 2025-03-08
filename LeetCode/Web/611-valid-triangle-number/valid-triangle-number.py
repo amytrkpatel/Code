e5635604d041ee8f19c6abc4d290bd0aeeb6f2df
Amit Patel <amytrkpatel20@gmail.com>
2025-03-08 06:49:31 +0530

    Time: 431 ms (57.19%) | Memory: 17.7 MB (96.47%) - LeetSync

--- LeetCode/Web/611-valid-triangle-number/valid-triangle-number.py
+++ LeetCode/Web/611-valid-triangle-number/valid-triangle-number.py
@@ -2,7 +2,7 @@ class Solution:
     def triangleNumber(self, nums: List[int]) -> int:
         nums.sort()
         combinations = 0
-        for c in range(len(nums)-1, -1, -1):
+        for c in range(len(nums)-1, 1, -1):
             left = 0
             right = c-1
             while left < right:    
