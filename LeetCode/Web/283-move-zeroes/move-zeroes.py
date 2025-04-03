93350f045653036f08f9e1446973df44bd15fd2c
Amit Patel <amytrkpatel20@gmail.com>
2025-04-03 05:33:07 +0530

    Time: 0 ms (100.00%) | Memory: 18.9 MB (49.42%) - LeetSync

--- LeetCode/Web/283-move-zeroes/move-zeroes.py
+++ LeetCode/Web/283-move-zeroes/move-zeroes.py
@@ -3,14 +3,15 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        i, j = 0, 1
 
-        i = 0
-
-        for j in range(len(nums)):
-            if nums[j] != 0:
-                nums[i] = nums[j]
+        while j < len(nums):
+            if nums[i] == 0 and nums[j] != 0: #01
+                nums[i], nums[j] = nums[j], nums[i]
                 i += 1
-        
-        while i < len(nums):
-            nums[i] = 0
-            i += 1
+                j += 1
+            elif nums[i] == 0 and nums[j] == 0: #00
+                j += 1
+            else: #00 and 10
+                i += 1
+                j += 1
